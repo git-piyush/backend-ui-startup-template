5c31310a2439756cfaf587c4d65bf06345c8d016
Piyush <kmrpiyush95@gmail.com>
2025-12-24 14:18:36 +0530

    test

--- frontend-app1/src/app/login/login.component.ts
+++ frontend-app1/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'app/service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
   }
 
+// Model to bind form values
+  loginData = {
+    email: '',
+    password: ''
+  };
+  error: any = null;
+  constructor(private authService: AuthService, 
+    private router: Router) {}
+
+   // Method to accept form values
+  onLogin(): void {
+    const { email, password } = this.loginData;
+
+    // For now, just log them
+    console.log('Username:', email);
+    console.log('Password:', password);
+    console.log(this.loginData);
+    this.authService.loginUser(this.loginData).subscribe({
+      next: (res:any) => {
+        if (res.status === 200) {
+          this.authService.storeToken('token', res.token);
+          this.authService.storeToken('role', res.role);
+          this.authService.storeToken('userName', res.userName);
+          if(this.authService.isAdmin()){
+              this.router.navigate(['/admindashboard']);
+          }else if(this.authService.isCustomer()){
+              this.router.navigate(['/customeradmin'])
+          }else if(this.authService.isManager()){
+              this.router.navigate(['/admindashboard'])
+          }
+          
+        }
+      },
+      error: (err: any) => {
+        this.showError(err?.error?.message || err.message || 'Unable To Login: ' + err)
+      }
+    });
+  }
+    showError(msg: string){
+    this.error = msg;
+    setTimeout(()=> {
+      this.error = null
+    }, 4000);
+  }
 }
